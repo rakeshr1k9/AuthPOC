f38d2d22bbe466260eb4d657b956291d63e082ab
Rakesh R <rakeshr1k9@gmail.com>
2019-01-20 13:14:39 +0530

    initial git copy

---
+++ src/main/java/in/ogmatech/AuthPOC/AuthPocApplication.java
@@ -0,0 +1,14 @@
+package in.ogmatech.AuthPOC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthPocApplication.class, args);
+	}
+
+}
+
